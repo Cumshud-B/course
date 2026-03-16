13f686d7bfcd610ee3596d05c7f4bb2a2c726b50
Cumshud-B <jumshudeb-apa103@code.edu.az>
2026-03-16 14:02:04 +0400

    course project

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Course
+﻿using Course.Models;
+
+namespace Course
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student("Jumshud", "Bakhtiyarli", "baktiyarli2006@gmail.com", "player123", "Cumsud", "+994 10 215 2006");
+            Console.WriteLine(student.GetName());
+
+            Student student1 = new Student("hemid", "Hamid", "Aliyev", "682a1", new DateOnly(2005, 03, 22), "hamidaliyev@gmail.com", "hamid123");
+            Console.WriteLine(student1.GetStudent());
+
+
+            //Student student2 = new Student()
         }
     }
 }
