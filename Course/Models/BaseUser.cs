bc5aa27bc9dd6fe5c4902c06a9f14be800a739da
Cumshud-B <jumshudeb-apa103@code.edu.az>
2026-03-18 13:09:45 +0400

    baseuser

---
+++ Course/Models/BaseUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Models
+{
+    public class BaseUser
+    {
+        public string Username { get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public string Adress { get; set; }
+        public string Phonenumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
